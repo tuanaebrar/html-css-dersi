4d2de7fa49c05773ab8da1a3a77d3575c8e78c95
tuanaebrar <tuanaebrar61@hotmail.com>
2018-08-08 09:42:55 +0300

    git ogreniyorum

---
+++ sublime text/kodadi2023.php
@@ -0,0 +1,17 @@
+<?php
+
+
+echo "TUANA";
+echo "<br>";
+echo "TUANA";
+
+echo "TUANA <br> TUANA";
+
+//Bunlar yorum satırı
+# Bunlar yorum satırı
+
+$kelime = "Kod Adı";
+$sayi = "2023";
+
+echo $kelime " " $sayi  ;
+ ?>  
